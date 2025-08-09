4d16ea11675dec1a81191a0b521fffc9b403eb1d
rflimat <raulflimat@gmail.com>
2025-08-08 19:17:59 -0500

    Funcion para agregar amigos creada

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.querySelector("#amigo").value;
+    
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+    } else if (amigos.includes(nombre)) {
+        alert("El nombre ya existe.");
+    } else {
+        amigos.push(nombre);
+    }
+
+    document.querySelector("#amigo").value = "";
+    
+    // actualizarListaAmigos();
+}
